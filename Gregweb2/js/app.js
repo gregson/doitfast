e7c982555f3ff22f980dc4627726292362c63b0c
Gregson <gregson@doitfast.com>
2025-03-17 09:22:24 -0700

    Ajout des fichiers du projet Gregweb2

---
+++ Gregweb2/js/app.js
@@ -0,0 +1,40 @@
+const triggerSideNav = document.querySelector('.side-nav-btn-open');
+const closeSideNav = document.querySelector('.side-nav-btn-close');
+const modal = document.getElementById('sideNav');
+const body = document.querySelector('body');
+const liElements = document.querySelectorAll('.container-nav li');
+
+triggerSideNav.addEventListener('click', event => {
+    modal.classList.add('show');
+    body.style.overflow = 'hidden'; // Ajoute overflow: hidden au body
+});
+
+closeSideNav.addEventListener('click', event => {
+    modal.classList.remove('show');
+    body.style.overflow = ''; // Réinitialise overflow du body
+});
+
+liElements.forEach(li => {
+    li.addEventListener('click', event => {
+        modal.classList.remove('show');
+        body.style.overflow = ''; // Réinitialise overflow du body    
+    });
+});
+
+
+function setEqualHeight() {
+    let slides = document.querySelectorAll('.carousel-item');
+    let maxHeight = Math.max(...Array.from(slides, slide => {
+        slide.style.height = '';
+        return slide.offsetHeight;
+    }));
+    slides.forEach(slide => slide.style.height = `${maxHeight}px`);
+}
+
+
+// Run the initialization
+setEqualHeight();
+
+// Delay initial call and add it to the end of the event queue
+window.addEventListener('resize', setEqualHeight);
+
